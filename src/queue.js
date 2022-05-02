b66d689f8e038a7b0153eb327eb4098596f62c28
Dimon22104 <75577050+Dimon22104@users.noreply.github.com>
2022-05-02 22:41:42 +0300

    Update queue.js

--- src/queue.js
+++ src/queue.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
@@ -13,22 +13,50 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
-class Queue {
+ class Queue {
+  // arr = []
+  // current = null
+  // obj = {value: null, next:null }
 
-  getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  constructor (){ 
+    this.len = null
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
+  getUnderlyingList() {
+     
+      // return this.obj
+      return this.len
+
+  }
+
+  enqueue(value) {
+      if(this.len === null){ 
+        this.len = new ListNode(value)
+        return this.len
+      }
+
+      let tar = this.len
+      while(tar.next !== null){ 
+        tar = tar.next
+      }
+      // NODE UPD
+      tar.next  = new ListNode(value)
     // remove line with error and write your code here
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
+      let tar = this.len
+      this.len = this.len.next
+
+      // console.log(this.current);
+      return tar.value
     // remove line with error and write your code here
   }
+
+  // print(){ 
+  //     console.log(this.arr)
+  // }
 }
 
 module.exports = {
